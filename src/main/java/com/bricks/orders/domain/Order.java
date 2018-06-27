34d965e354e6b447d2d8d1fb441b14e4b6ee3a60
Sina <s.aleyaasin@googlemail.com>
2018-06-28 00:30:51 +0100

    Implement Order Bean
    Signed-off-by: Sina <s.aleyaasin@googlemail.com>

--- src/main/java/com/bricks/orders/domain/Order.java
+++ src/main/java/com/bricks/orders/domain/Order.java
@@ -1,8 +1,42 @@
 package com.bricks.orders.domain;
 
+
+import org.springframework.data.annotation.Id;
+
 public class Order {
 
+    @Id
+    private String id;
+
+    private long quantity;
+
+    public Order() {
+
+    }
+
+    public Order(String  id, long quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+
     public String getId() {
-        return null;
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s[id=\"%s\", quantity=%d]", getClass().getName(), id, quantity);
     }
 }
